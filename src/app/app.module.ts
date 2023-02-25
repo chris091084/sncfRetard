a5a92471d44f1f664325741177820cad7be195e3
christian <christian.coley84@gmail.com>
2023-02-25 20:07:08 +0100

    add/form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormDeclarationComponent } from './form-declaration/form-declaration.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormDeclarationComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FormDeclarationComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/form-declaration/form-declaration.component.ts
+++ src/app/form-declaration/form-declaration.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-declaration',
   templateUrl: './form-declaration.component.html',
-  styleUrls: ['./form-declaration.component.scss']
+  styleUrls: ['./form-declaration.component.scss'],
 })
 export class FormDeclarationComponent {
+  formDeclaration = new FormGroup({
+    date: new FormControl(new Date(), Validators.required),
+    station: new FormControl('', Validators.required),
+    trainNumber: new FormControl(''),
+    problem: new FormControl('', Validators.required),
+    comment: new FormControl(''),
+  });
 
+  constructor() {}
+
+  onSubmit() {
+    console.log(this.formDeclaration.value);
+  }
 }
